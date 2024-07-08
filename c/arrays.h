5138528329222928bf4401ec2581bfa2337e8055
Mariela <mariela.hernandezchacon@ucr.ac.cr>
2024-07-08 17:02:38 -0600

    Files for exam 2

---
+++ c/arrays.h
@@ -0,0 +1,12 @@
+#ifndef ARRAYS_H
+#define ARRAYS_H
+
+#include <stdbool.h>
+
+// Definición del struct arreglo_t
+
+// Declaración de funciones
+void sortArray(int* arr, int size);
+void llenarAray(arreglo_t* arr_struct);
+
+#endif // ARRAYS_H
